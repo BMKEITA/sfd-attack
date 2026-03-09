0b2aa6383e7bce31feb759dda67176f9f9bbf8a5
Boubacarmkeita <129256151+BMKEITA@users.noreply.github.com>
2026-03-09 17:17:50 +0800

    Update __init__.py

--- attacks/__init__.py
+++ attacks/__init__.py
@@ -1,1 +1,21 @@
+"""
+Attack modules initialization.
+"""
 
+from attacks.base import BaseAttack, SpatialOnlyAttack, FrequencyOnlyAttack, HybridAttack
+from attacks.hybrid import SequentialHybridAttack, JointHybridAttack, AdaptiveBandAttack
+from attacks.sota import FGSM, PGD, SFA, FACL
+
+__all__ = [
+    'BaseAttack',
+    'SpatialOnlyAttack',
+    'FrequencyOnlyAttack',
+    'HybridAttack',
+    'SequentialHybridAttack',
+    'JointHybridAttack',
+    'AdaptiveBandAttack',
+    'FGSM',
+    'PGD',
+    'SFA',
+    'FACL',
+]
